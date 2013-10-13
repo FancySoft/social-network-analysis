22d12b2b93cc4cf11e1d1ee398e84b2a3965f0b1
Desman <artem.kirienko@gmail.com>
2013-10-13 15:39:36 +0400

    Matcher changed to work from the command line.

--- src/com/fancy_software/accounts_matching/matcher/TestMain.java
+++ src/com/fancy_software/accounts_matching/matcher/TestMain.java
@@ -4,10 +4,23 @@ import java.io.FileNotFoundException;
 
 public class TestMain {
 
+    private static final String PATH_FROM = "accounts/vk";
+    private static final String PATH_TO = "accounts/social_network2";
+
     public static void main(String args[]) throws FileNotFoundException {
-        //don't commit
+        String pathFrom;
+        String pathTo;
+        if (args.length < 2) {
+            pathFrom = PATH_FROM;
+            pathTo = PATH_TO;
+        }
+        else {
+            pathFrom = args[0];
+            pathTo = args[1];
+        }
+
         AccountMatcher matcher = new AccountMatcher();
-        matcher.init("accounts/vk/", "accounts/social_network2/");
+        matcher.init(pathFrom, pathTo);
         matcher.match(false);
     }
 }
