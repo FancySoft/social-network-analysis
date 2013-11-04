10dba38be259342819cb13e6aaf2c00328efaca5
Desman <artem.kirienko@gmail.com>
2013-11-05 00:38:18 +0400

    Booster modification

--- src/com/fancy_software/accounts_matching/matcher/Booster.java
+++ src/com/fancy_software/accounts_matching/matcher/Booster.java
@@ -2,10 +2,7 @@ package com.fancy_software.accounts_matching.matcher;
 
 import com.fancy_software.accounts_matching.model.AccountVector;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * ************************************************************************
@@ -16,23 +13,31 @@ import java.util.Map;
 public class Booster implements IMatcher {
 
     private Collection<IMatcher> matchers;
+    private Map<IMatcher, Double> weights;
 
-    public Booster() {
-        // init matchers
-        matchers = new ArrayList<IMatcher>();
+    public Booster(List<IMatcher> matchers) {
+        this.matchers = matchers;
+        initWeights();
+    }
+
+    private void initWeights() {
+        weights = new HashMap<IMatcher, Double>(matchers.size());
+        for (IMatcher matcher : matchers) {
+            weights.put(matcher, 1.);
+        }
     }
 
     @Override
     public AccountVector match(AccountVector goal) {
-        Map<AccountVector, Integer> results = new HashMap<AccountVector, Integer>(matchers.size());
+        Map<AccountVector, Double> results = new HashMap<AccountVector, Double>(matchers.size());
         AccountVector cur = null;
         for (IMatcher matcher : matchers) {
             cur = matcher.match(goal);
-            if (results.containsKey(cur)) results.put(cur, results.get(cur) + 1);
-            else results.put(cur, 1);
+            if (results.containsKey(cur)) results.put(cur, results.get(cur) + weights.get(matcher));
+            else results.put(cur, weights.get(matcher));
         }
 
-        int max = 0;
+        Double max = 0.;
         for (AccountVector vector : results.keySet()) {
             if (results.get(vector) > max) {
                 max = results.get(vector);
