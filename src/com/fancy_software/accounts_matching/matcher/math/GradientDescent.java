21bd297a3a35a76782814d2d11bf98660e26c869
EmoCoder <artem.kirienko@gmail.com>
2013-11-13 20:23:55 +0400

    GradientDescent implementation

--- src/com/fancy_software/accounts_matching/matcher/math/GradientDescent.java
+++ src/com/fancy_software/accounts_matching/matcher/math/GradientDescent.java
@@ -11,6 +11,13 @@ import com.fancy_software.logger.Log;
 public class GradientDescent {
     private static final String TAG = GradientDescent.class.getSimpleName();
 
+    /**
+     * Приближенное значение градиента
+     * @param f  функция
+     * @param v1 начальная точка
+     * @param v2 конечная точка
+     * @return   вектор-градиент
+     */
     public static double[] grad(IFunction f, double[] v1, double[] v2) {
         if (v1.length != v2.length) {
             Log.e(TAG, "Different dimensions!");
@@ -29,8 +36,81 @@ public class GradientDescent {
         return result;
     }
 
+    /**
+     * Сложение векторов
+     * @param v1 вектор
+     * @param v2 вектор
+     * @return   сумма
+     */
+    public static double[] add(double[] v1, double[] v2) {
+        int dim = v1.length;
+        double result[] = new double[v1.length];
+        for (int i = 0; i < dim; i++) {
+            result[i] = v1[i] + v2[i];
+        }
+        return result;
+    }
+
+    /**
+     * Вычитание векторов
+     * @param v1 из вектора v1
+     * @param v2 вычитается v2
+     * @return   разность
+     */
+    public static double[] subtract(double[] v1, double[] v2) {
+        int dim = v1.length;
+        double result[] = new double[v1.length];
+        for (int i = 0; i < dim; i++) {
+            result[i] = v1[i] - v2[i];
+        }
+        return result;
+    }
+
+    /**
+     * Нормализация вектора (деление на меру и на step)
+     * @param v вектор
+     * @return  нормализованный вектор
+     */
+    private static double[] normalize(double[] v) {
+        double measure = 0;
+        double result[] = new double[v.length];
+        for (double x : v) {
+            measure += x*x;
+        }
+        for (int i = 0; i < v.length; i++) {
+            result[i] = v[i] / (measure * step);
+        }
+        return result;
+    }
+
+    private static int step;
+
+    /**
+     * Минимизация f методом градиентного спуска
+     * @param f    функция
+     * @param init начальная точка
+     * @return     локальный минимум
+     */
     public static double[] minimize(IFunction f, double[] init) {
-        // TODO: follow negative gradient
-        return init;
+        step = 2;
+        int dim = init.length;
+
+        double[] direction = new double[dim];
+        for (int i = 0; i < dim; i++) {
+            direction[i] = 1. / i;
+        }
+
+        double[] prev;
+        double[] cur = add(init, direction);
+
+        int ITERATIONS = 5;
+        while (ITERATIONS > 0) {
+            direction = normalize(grad(f, init, cur));
+            prev = cur;
+            cur = subtract(prev, direction);
+            step *= 2;
+        }
+
+        return cur;
     }
 }
