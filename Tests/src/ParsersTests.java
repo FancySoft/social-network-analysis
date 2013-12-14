716b25a2cdecf1166e9b35093dafb800ad3a2d8e
Desman <artem.kirienko@gmail.com>
2013-12-14 21:36:22 +0400

    Tests added

--- Tests/src/ParsersTests.java
+++ Tests/src/ParsersTests.java
@@ -26,4 +26,20 @@ public class ParsersTests {
         goal.setBdate(new BirthDate(16, 3, 1992));
         Assert.assertNotNull(parser.match(goal));
     }
+
+    @Test
+    public void testMatch() {
+        AbstractParser parser = ParserFactory.getApiWorkerInstance(SocialNetworkId.VK);
+        Settings settings = Settings.getInstance();
+        parser.auth(settings.get("vk_login"), settings.get("vk_password"));
+        AccountVector goal = new AccountVector();
+        goal.setFirst_name("Artem");
+        goal.setLast_name("Kirienko");
+        goal.setBdate(new BirthDate(16, 3, 1992));
+        BirthDate resDate = new BirthDate(16, 3, 1992);
+        Assert.assertEquals(parser.match(goal).getFirst_name(), "Artem");
+        Assert.assertEquals(parser.match(goal).getLast_name(), "Kirienko");
+        Assert.assertEquals(parser.match(goal).getBdate(), resDate);
+        Assert.assertEquals(parser.match(goal).getSex(), 2);
+    }
 }
